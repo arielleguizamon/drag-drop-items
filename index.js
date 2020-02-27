4fd2db5b6b65b5566c71af150ae77cec5b968d32
Ariel Leguizamon <a.leguizamonariel@gmail.com>
2020-02-26 23:17:50 -0300

    wip

---
+++ index.js
@@ -0,0 +1,126 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+var bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://mongo:27017/challenge", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+/****************** Model  ******************/
+
+const Item = mongoose.model("Item", {
+  description: String,
+  file: String,
+  position: Number
+});
+
+/****************** File Upload  ******************/
+
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/img");
+  },
+
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+const upload = multer({ storage, fileFilter: imageFilter }).single("file");
+
+/****************** Endpoints ******************/
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/front/index.html");
+});
+
+app.post("/api/items", (req, res) => {
+  upload(req, res, async err => {
+    if (err || !req.file || !req.body.description) {
+      return res.status(400).json("Error");
+    } else {
+      const count = await Item.count();
+      const item = new Item({
+        description: req.body.description,
+        position: count,
+        file: req.file.filename
+      });
+      try {
+        const saved = await item.save();
+        res.status(201).json(saved);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+    }
+  });
+});
+
+app.put("/api/items/:id", async (req, res) => {
+  const id = req.params.id;
+  upload(req, res, async err => {
+    if (err) {
+      return res.status(400).json("Error", err);
+    } else {
+      const item = await Item.findById(id);
+      if (req.file) item.file = req.file.filename;
+      item.description = req.body.description;
+      try {
+        const saved = await item.save();
+        res.status(200).json(saved);
+      } catch (err) {
+        res.status(400).json(err);
+      }
+    }
+  });
+});
+
+app.get("/api/items", async (req, res) => {
+  const items = await Item.find().sort("position");
+  res.status(200).json(items);
+});
+
+app.post("/api/items/:id/position", async (req, res) => {
+  const item = await Item.findByIdAndUpdate(
+    req.params.id,
+    { position: req.body.position },
+    { new: true }
+  );
+  return res.status(200).json(item);
+});
+
+app.post("/api/items/positions", async (req, res) => {
+  const { from, to, type } = req.body;
+  const operator = type === "add" ? 1 : -1;
+  const items = await Item.updateMany(
+    { position: { $gte: from, $lte: to } },
+    { $inc: { position: operator } }
+  );
+  return res.status(200).json(items);
+});
+
+app.delete("/api/items/:id", async (req, res) => {
+  await Item.findByIdAndDelete(req.params.id);
+  return res.status(204).json();
+});
---
+++ public/front/script.js
@@ -0,0 +1,139 @@
+new Sortable(list, {
+  animation: 150,
+  onEnd: async evt => {
+    // Get id from item
+    if (evt.newIndex === evt.oldIndex) return;
+    // if we are doing a drag and drop to the top
+    if (evt.oldIndex > evt.newIndex)
+      await updatePositions(evt.newIndex, evt.oldIndex - 1, "add");
+    // if we are doing a drag and drop to the bottom
+    else if (evt.oldIndex < evt.newIndex)
+      await updatePositions(evt.oldIndex + 1, evt.newIndex, "rest");
+
+    await updateItemPosition(evt.item.id, evt.newIndex);
+  }
+});
+
+const updatePositions = async (from, to, type) => {
+  const response = await fetch("http://localhost:3000/api/items/positions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ from, to, type })
+  });
+};
+
+const updateItemPosition = async (id, position) => {
+  const response = await fetch(
+    `http://localhost:3000/api/items/${id}/position`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ position })
+    }
+  );
+};
+
+const setLength = length =>
+  (document.getElementById("count").textContent = length);
+
+const deleteItem = async id => {
+  document.getElementById(id).remove();
+  const response = await fetch(`http://localhost:3000/api/items/${id}`, {
+    method: "DELETE"
+  });
+  setLength();
+};
+const openModal = id => {
+  $("#modal").modal();
+  if (id) {
+    const description = $(`#description-${id}`).text();
+    $("#description").val(description);
+    $("#id").val(id);
+    $("#file").val("");
+  } else {
+    $("#description").val("");
+    $("#file").val("");
+    $("#id").val(undefined);
+  }
+};
+
+$("#form").submit(event => {
+  event.preventDefault();
+
+  const fileInput = document.getElementById("file");
+  const file = fileInput.files[0];
+  const id = $("#id").val();
+
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("description", $("#description").val());
+  if (id) updateItem(id, formData);
+  else createItem(formData);
+});
+
+const updateItem = async (id, formData) => {
+  const response = await fetch(`http://localhost:3000/api/items/${id}`, {
+    method: "PUT",
+    body: formData
+  });
+  if (response.status >= 400 && response.status < 600)
+    alert(
+      "There was an error creating the item, make sure you are uploading an image"
+    );
+  else {
+    const item = await response.json();
+    $(`#description-${item._id}`).text(item.description);
+    document.getElementById(`img-${item._id}`).src = `/img/${item.file}`;
+    $("#modal").modal("hide");
+  }
+};
+
+const createItem = async formData => {
+  const response = await fetch("http://localhost:3000/api/items", {
+    method: "POST",
+    body: formData
+  });
+  if (response.status >= 400 && response.status < 600)
+    alert(
+      "There was an error creating the item, make sure you are uploading an image"
+    );
+  else {
+    const item = await response.json();
+    createAndInsertItem(item);
+    $("#modal").modal("hide");
+  }
+};
+
+const createAndInsertItem = item => {
+  const newItemHtml = newItem(item);
+  const list = document.getElementById("list");
+  list.insertAdjacentHTML("beforeend", newItemHtml);
+};
+
+const newItem = item =>
+  `<div class="list-group-item" id="${item._id}">
+    <img id="img-${item._id}" src="/img/${item.file}" height="80px" width="80px"/>
+    <span onclick="openModal('${item._id}')" id="description-${item._id}">
+    ${item.description}
+    </span>
+    <span class="icons">
+      <i class="far fa-edit" onclick="openModal('${item._id}')"></i>
+      <i class="far fa-trash-alt" onclick="deleteItem('${item._id}')"></i>
+    </span>
+  </div>`;
+
+const getItems = async () => {
+  const response = await fetch("http://localhost:3000/api/items");
+  const items = await response.json();
+  setLength(items.length);
+  items.forEach(e => {
+    createAndInsertItem(e);
+  });
+};
+
+getItems();
+// setLength();
